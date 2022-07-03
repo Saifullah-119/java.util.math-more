7ce619a5e8cd277714a98f2b5c0e1ac372ebbd0d
Saifullah-119 <104373970+Saifullah-119@users.noreply.github.com>
2022-07-03 19:50:28 +0500

    Add files via upload

---
+++ MainMeth.java
@@ -0,0 +1,8 @@
+
+class MainMeth
+{
+	public static void main(String[] args)
+	{
+		System.out.println("abs(-2): " +Math.abs(-2)+ "addExact(-3,4): " +Math.addExact(-3,4)+ "cbrt(27): " +Math.cbrt(27)+ "ceil(27.2): " +Math.ceil(27.2)+ "floor(27.2): " + Math.floor(27.2)+ "copySign(27.2,-3): " + Math.copySign(27.2,-3));
+	}
+}
